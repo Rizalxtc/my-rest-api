6721dce21914758635b8675488dbbc4f590d84c7
Rizalxtc <115970773+Rizalxtc@users.noreply.github.com>
2023-07-13 10:26:59 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,9 +2,9 @@ const fs = require('fs')
 
 global.creator = 'Rizal Codder'// yourname
 global.MONGO_DB_URI = "mongodb+srv://rizalbot:Gamer999@rizalmods.10yiu9p.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
-global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
+global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@/1jAPPnew/@10" //isi apa aja bebas
 global.your_email = "sibolangbolang529@gmail.com" //email
-global.email_password = "nfyodearbpqgiqdj" //application password email
+global.email_password = "lnqmdoupnaclprpi" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
